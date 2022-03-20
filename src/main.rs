7cdf10b44b9f789440e31bd2ff46dcf747563fb4
apie <Epiphanyaggregator@protonmail.com>
2022-03-20 12:54:18 -0700

    Added tests, random port by default, and a health_check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+//! src/main.rs
+use z2p::run;
+use std::net::TcpListener;
+
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:0")
+    .expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    println!("Server started on port {}", port);
+
+    run(listener)?.await
 }
